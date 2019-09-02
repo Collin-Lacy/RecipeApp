6e5561d546c9a4606f3b41e9cc921f9b313dba2e
Collin-Lacy <clacy@wisc.edu>
2019-09-02 00:57:15 +0000

    Add jest, eslinter, and webpack aliasing

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,18 @@ module.exports = {
         path : path.resolve(__dirname , 'dist'),
         filename: 'index_bundle.js'
     },
+    resolve: {
+        alias: {
+            '@components': path.resolve(__dirname, 'src/components/'),
+            '@hooks': path.resolve(__dirname, 'src/hooks/'),
+            '@containers': path.resolve(__dirname, 'src/containers'),
+            '@validations': path.resolve(__dirname, 'src/validations/'),
+            '@views': path.resolve(__dirname, 'src/views/')
+        }
+    },
     module : {
         rules : [
-            {test : /\.(js)$/, use:'babel-loader'},
+            {test : /\.(js)$/, use: ['babel-loader', 'eslint-loader']},
             {test : /\.css$/, use:['style-loader', 'css-loader']}
         ]
     },
@@ -19,5 +28,4 @@ module.exports = {
 	    template: path.resolve('src', 'templates', 'index.html'),
         })
     ]
-
 }
